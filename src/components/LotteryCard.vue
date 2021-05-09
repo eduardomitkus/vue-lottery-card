57665ac69b0b4a80e5ed2b5ec63733b39f6aa431
Eduardo Mitkus <eduardo.mitkus@gmail.com>
2021-05-09 18:14:09 -0300

    Adiciona componente para o cartão da loteria onde receberá os números

---
+++ src/components/LotteryCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <div class="card-content">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoteryCard'
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="home">
-  </div>
+  <lottery-card> 
+  </lottery-card>
 </template>
 
 <script>
+import LotteryCard from '../components/LotteryCard'
 
 export default {
   name: 'Home',
   components: {
+    LotteryCard
   }
 }
 </script>
