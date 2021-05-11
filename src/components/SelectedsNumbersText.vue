f179207f20658ff64f3be5c7874faf0ce97b618d
Eduardo Mitkus <eduardo.mitkus@gmail.com>
2021-05-11 02:27:12 -0300

    Adiciona validação para mensagem de exibição de dezenas

--- src/components/SelectedsNumbersText.vue
+++ src/components/SelectedsNumbersText.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="center row grey-text text-darken-2">
+    <div v-if="numbersSelecteds.length > 0">
       <span>Voce jogou as seguintes dezenas: </span>
       <span v-for="(numberSelected, key) in numbersSelecteds" :key="key">
         {{ additionalCharacter(key)+numberSelected }}
       </span>
+    </div>
+    <div v-else>
+      Você ainda não marcou nenhuma dezena
+    </div>
+      
   </div>
 </template>
 
