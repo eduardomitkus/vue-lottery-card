e23c55163fa6d85036718debe659b05e1d0a4ca5
Eduardo Mitkus <eduardo.mitkus@gmail.com>
2021-05-11 02:05:00 -0300

    Adicionado componente para exibir texto de números selecionados

---
+++ src/components/SelectedsNumbersText.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="center row grey-text text-darken-2">
+      <span>Voce jogou as seguintes dezenas: </span>
+      <span v-for="(numberSelected, key) in numbersSelecteds" :key="key">
+        {{ additionalCharacter(key)+numberSelected }}
+      </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SelectedsNumbersText',
+  props: {
+    numbersSelecteds: Array,
+  },
+  methods: {
+    additionalCharacter(key) {
+      return key === 0 ? '' : '+'
+    }
+  }
+}
+</script>
